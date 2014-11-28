e4fe0a2766c91c8adffec5b2ad52081987dea8c2
MetteOW <metteowinther@gmail.com>
2014-11-28 14:11:54 +0100

    Alle sider
    Her er alle de sider vi lavede i vores site-map

---
+++ aktiviteter.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<main>
+ <header>
+	<?php include('includes/menu_inc.php');?>
+ </header>
+<section></section>
+<footer></footer>
+
+
+</main>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
