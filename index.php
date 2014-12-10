f5f8a4d22dabe3d22cd4e2508169be2f405d35d2
carstenholmclausen <chc-@hotmail.com>
2014-12-10 14:29:20 +0100

    test
    ...

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 Velkommen til kursusstedet Materia.
 Her udbydes kurser i mange gamle håndværk, fx smedning, træarbejde og friluftsliv.
 Kurserne henvender sig til skoler, institutioner og private.
-Vi tror naivt på, at den manuelle historie og det skabende håndværk ligger dybt i os alle, man skal bare finde derind.
+Vi tror naivt på, at den manuelle historie og det skabende håndværk ligger dybt i os alle, man skal bare finde derind.....
 </p>
 </section>
 <?php include('includes/footer_inc.php');?>
