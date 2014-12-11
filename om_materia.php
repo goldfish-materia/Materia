4ccbc8ce6501a1ff81519f7f75cccfa4f9b0bb51
xzoras <msoholm@msn.com>
2014-12-11 16:40:11 +0100

    Om materia
    opsætning af personer rettet til / Michelle

--- om_materia.php
+++ om_materia.php
@@ -1,70 +1,121 @@
 <?php include('includes/header_inc.php');?>
-<main>
-<article class="ommateria">
-	<h1>Om Materia</h1><br>
-<h2>Hvem vi er</h2>
-<p>
-Kursuscentret Materia, ejes og drives af skoleleder Peter Engelbrekt Knøss fra Randers Lilleskole, samt hans kone Gitte Lautrup, der arbejder som adjunkt i gymnasieskolen. Her foruden er løst tilknyttet en række personer med stærke kompetencer på forskellige felter.
-<br>
-<h2>Navnet Materia</h2>
-<p>
-Navnet Materia er inspireret af filosoffen Aristoteles´ tanker om hvordan verden er opbygget. Materia var en betegnelse for uformet stof og på Materia former vi materialer.
-Materia kaldes også Materialernes Verden fordi der arbejdes med mange materialer. Da Materia ligger på Kristrup Engvej, omtales stedet også som dette.
-<br>
-<h2>Vores historie</h2>
-<p>
-For 20 år siden startede vi som en kursusvirksomhed, som kørte rundt i Danmark og lavede kurser i smedning, grov smedning, dukkebygning og bronzestøbning.
-I 2005 købte vi 23.000m2 jord ved Randers, som nu er vores faste undervisningssted. Her er smedje, 2 pavilloner, huggehus, bål hus, shelter, 3 huse i lille skov og toiletter.
-<br>
-<h2>Materias undervisere</h2>
-<p>
-Kursusstedets undervisere er de dygtigste på hver sit felt, det betyder, at alle kurser er på et meget højt niveau. Kursusstedet har tilknyttet 5 forskellige undervisere, men ejeren Peter Knøss sørger for den daglige drift.
-</p>
-</article>
-<br>
-<article class="peterknoss">
-<img class="personfotos" src="images/peterKnoss.png">
-<aside class="undervisertekst"><br>
-<h3>Peter Knøss</h3>
-<p>Jeg er lærer i bl.a. dansk og sløjd og har arbejdet med dette gennem mange år. Desuden har jeg en Pædagogisk diplomuddannelse i sløjd og en cand. pæd i materiel kultur. Gennem tolv år har jeg arbejdet som lektor på læreruddannelsen i Århus, men valgte i 2013 at foretage et karriereskifte. I dag arbejder jeg som skoleleder på Randers Lilleskole.</p></aside>
-</article>
-<br>
-<article class="klauspoulsen">
-<img class="personfotos" src="images/klausPoulsen.png">
-<aside class="undervisertekst">
-<h3>Klaus Poulsen</h3>
-<p>Jeg er 45 år, uddannet lærer og pt selvstændig med firmaet ”Æstetikfabrikken”. Jeg udfører hovedsaligt tømrer- og især snedkeropgaver og designer møbler.
-Udover det afholder jeg undervisningsforløb og temadage på Materia. Det kan være undervisning af folkeskole- og efterskoleelever i smedning, grøn sløjd, design og madlavning i det fri. Mht. til temadagene er det typisk firmaer der gerne vil prøve kræfter med nogle af disse discipliner, samtidigt med at de hygger sig. Det kan også være deciderede sociale arrangementer, hvor jeg tilbereder maden. Læs om <a href="https://www.facebook.com/AEstetikfabrikken?pnref=lhc">Æstetikfabrikken </a>her</p></aside>
-</article>
-<br>
-<article class="troelsnyholt">
-<img class="personfotos" src="images/troelsNyholt.png">
-<aside class="undervisertekst">
-<h3>Troels Nyholt</h3>
-<p>Her skal stå information om Troels</p></aside>
-</article>
-<br>
-<article class="andreas">
-<img class="personfotos" src="images/personAndreas.png">
-<aside class="undervisertekst">
-<h3>Andreas</h3>
-<p>Her skal stå information om Andreas</p></aside>
-</article>
-<br>
-<article class="pia">
-<img class="personfotos" src="images/personPia.png">
-<aside class="undervisertekst">
-<h3>Pia</h3>
-<p>Her skal stå information om Pia. Hendes speciale er gummitasker og pil</p></aside>
-</article>
-<br>
-<article class="soren">
-<img class="personfotos" src="images/personSoren.png">
-<aside class="undervisertekst">
-<h3>Søren</h3>
-<p>Her skal stå information om Søren. Hans speciale er madlavning.</p></aside>
-</article>
-</main>
+<section>
+
+		<article>
+			<h1>Om Materia</h1>
+			<h2>Hvem vi er</h2>
+			<p>
+				Kursuscentret Materia, ejes og drives af skoleleder Peter Engelbrekt Knøss fra Randers Lilleskole, samt hans kone Gitte Lautrup, 
+				der arbejder som adjunkt i gymnasieskolen. Her foruden er løst tilknyttet en række personer med stærke kompetencer på forskellige felter.
+			</p>
+		
+
+			<h2>Navnet Materia</h2>
+			<p>
+			Navnet Materia er inspireret af filosoffen Aristoteles´ tanker om hvordan verden er opbygget. Materia var en betegnelse for uformet stof og 
+			på Materia former vi materialer. Materia kaldes også Materialernes Verden fordi der arbejdes med mange materialer. Da Materia ligger på Kristrup Engvej, omtales stedet også som dette.
+			</p>
+			
+			<h2>Vores historie</h2>
+			<p class="space">
+			For 20 år siden startede vi som en kursusvirksomhed, som kørte rundt i Danmark og lavede kurser i smedning, grov smedning, 
+			dukkebygning og bronzestøbning. I 2005 købte vi 23.000m2 jord ved Randers, som nu er vores faste undervisningssted. Her er smedje, 
+			2 pavilloner, huggehus, bål hus, shelter, 3 huse i lille skov og toiletter.
+			</P>
+		</article>
+
+
+
+		<article>
+			<h1>Materias undervisere</h1>
+			<p>
+				Kursusstedets undervisere er de dygtigste på hver sit felt, det betyder, at alle kurser er på et meget højt niveau. 
+				Kursusstedet har tilknyttet 5 forskellige undervisere, men ejeren Peter Knøss sørger for den daglige drift.
+			</p>
+		</article>
+
+		<article class="personfotos">
+			<img src="images/peterKnoss.png">
+		</article>
+		<article class="persontekst">
+			<h3>Peter Knøss</h3>
+			<p>
+				Jeg er lærer i bl.a. dansk og sløjd og har arbejdet med dette gennem mange år. 
+				Desuden har jeg en Pædagogisk diplomuddannelse i sløjd og en cand. pæd i materiel kultur. 
+				Gennem tolv år har jeg arbejdet som lektor på læreruddannelsen i Århus, men valgte i 2013 at foretage et karriereskifte. 
+				I dag arbejder jeg som skoleleder på Randers Lilleskole.
+			</p>
+		</article>
+
+
+
+		<article class="personfotos">
+			<img src="images/klausPoulsen.png">
+		</article>
+		<article class="persontekst">
+			<h3>Klaus Poulsen</h3>
+			<p>
+				Jeg er 45 år, uddannet lærer og pt selvstændig med firmaet ”Æstetikfabrikken”. Jeg udfører hovedsaligt tømrer- og især snedkeropgaver 
+				og designer møbler. Udover det afholder jeg undervisningsforløb og temadage på Materia. 
+				Det kan være undervisning af folkeskole- og efterskoleelever i smedning, grøn sløjd, design og madlavning i det fri. 
+				Mht. til temadagene er det typisk firmaer der gerne vil prøve kræfter med nogle af disse discipliner, samtidigt med at de hygger sig. 
+				Det kan også være deciderede sociale arrangementer, hvor jeg tilbereder maden. 
+				Læs om <a href="https://www.facebook.com/AEstetikfabrikken?pnref=lhc">Æstetikfabrikken </a>her
+	
+			</p>
+		</article>
+
+
+
+		<article class="personfotos">
+			<img src="images/troelsNyholt.png">
+		</article>
+		<article class="persontekst">
+			<h3>Troels Nyholt</h3>
+			<p>
+				Her skal stå information om Troels
+	
+			</p>
+		</article>
+
+
+
+		<article class="personfotos">
+			<img src="images/personAndreas.png">
+		</article>
+		<article class="persontekst">
+			<h3>Andreas</h3>
+			<p>
+				Her skal stå information om Troels
+	
+			</p>
+		</article>
+
+
+
+		<article class="personfotos">
+			<img src="images/personPia.png">
+		</article>
+		<article class="persontekst">
+			<h3>Pia</h3>
+			<p>
+				Her skal stå information om Pia. Hendes speciale er gummitasker og pil	
+			</p>
+		</article>
+
+
+
+		<article class="personfotos">
+			<img src="images/personSoren.png">
+		</article>
+		<article class="persontekst">
+			<h3>Søren</h3>
+			<p>
+				Her skal stå information om Søren. Hans speciale er madlavning.
+	
+			</p>
+		</article>
+</section>
 
 <?php include('includes/footer_inc.php');?>
 
