79660b1aa73f1459867a68f2ba87d5512628ffb9
Camillanederby <camillanederby@gmail.com>
2014-12-12 12:38:01 +0100

    forside
    forside rettet - ikke færdig!

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php include('includes/header_inc.php');?>
 <section>
-	<h1>Materia - Forside</h1>
-<p>
-Velkommen til kursusstedet Materia.
+	<h1 class="center">Materia</h1>
+    
+<p class="forside">
+Velkommen til kursusstedet Materia.</p>
+<p class="center">
+<img class="forside_træ" src="galleri/galleri_061.jpg">
+<img class="forside_metal" src="galleri/galleri_041.jpg">
+<img class="forside_ude" src="galleri/galleri_381.jpg">
+</p>
+<p class="træarbej">Træarbejde</p
+><p class="metalarbej">Metalarbejde</p>
+<p class="udeliv">Udeliv</p>
+
+<p class="undertext">
 Her udbydes kurser i mange gamle håndværk, fx smedning, træarbejde og friluftsliv.
-Kurserne henvender sig til skoler, institutioner og private.
-Vi tror naivt på, at den manuelle historie og det skabende håndværk ligger dybt i os alle, man skal bare finde derind.
+Kurserne henvender sig til skoler, institutioner og private.</p>
+<p class="undertext">Vi tror naivt på, at den manuelle historie og det skabende håndværk ligger dybt i os alle, man skal bare finde derind.
 </p>
 </section>
 <?php include('includes/footer_inc.php');?>
