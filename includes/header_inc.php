086863833bde213ac178ba4c5e0598d14bbed6fa
xzoras <msoholm@msn.com>
2014-12-15 14:26:25 +0100

    Galleri slider
    Galleri er tilføjet

--- includes/header_inc.php
+++ includes/header_inc.php
@@ -5,6 +5,7 @@
 <title>Materia</title>
 <link rel="stylesheet" type="text/css" href="css/reset.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/slider.css">
 <link href='http://fonts.googleapis.com/css?family=roboto' rel='stylesheet' type='text/css'>
 <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
 <script type="text/javascript" src="js/jquery-1.2.6.min.js"></script>
