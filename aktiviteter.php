f096e7fa7e3edc3d7b634035aa53efb6f2d5c34e
MetteOW <metteowinther@gmail.com>
2014-12-15 09:53:30 +0100

    Fotos ændret

--- aktiviteter.php
+++ aktiviteter.php
@@ -6,10 +6,11 @@
 
 	<h1>Aktiviteter</h1>
 	<p>På Materia afholder vi meget forskelligt. Se mere under menuerne</p>
-	<img class="aktivitetworkshop" src="images/workshopmini.png">
-    <img class="aktivitetkursus" src="images/stenhugningmini.png">
-    <img class="aktivitetsmedefestival" src="images/smedefestivalmini.png">
-    <img class="aktivitetnordisk" src="images/nordic2016mini.png"
+	<a href="workshops.php"<img class="aktivitetworkshop" src>
+   <img class="aktivitetworkshop" src="images/workshop.png"
+    <img class="aktivitetkursus" src="images/kreainnoentre.png">
+    <img class="aktivitetsmedefestival" src="images/smedefestival.png">
+    <img class="aktivitetnordisk" src="images/nordic2016.png">
     
 </section>
 <?php include('includes/footer_inc.php');?>
